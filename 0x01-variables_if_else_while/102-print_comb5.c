1defcc7bd62574483e4fc3e4039533ffda950eec
vantwill <101398976+vantwill@users.noreply.github.com>
2022-04-08 14:31:00 -0700

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,11 +12,11 @@ int main(void)
 	int t;
 	int o;
 
-	for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
+	for (tens = '0'; tens <= '9'; tens++)
 	{
 		for (ones = '0'; ones <= '9'; ones++)
 		{
-		 	for (t = tens; t <= '9'; t++) 
+		 	for (t = tens; t <= '9'; t++)
 			{
 				for (o = ones + 1; o <= '9'; o++)
 				{

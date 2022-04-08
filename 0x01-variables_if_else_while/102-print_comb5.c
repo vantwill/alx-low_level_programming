f8972237c5bbd54fc80930d8693c1c4b0bc52761
vantwill <vtwill41@gmail.com>
2022-04-08 14:24:36 -0700

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,50 +1,85 @@
 #include <stdio.h>
 
+
+
 /**
-<<<<<<< HEAD
- *main - print a num pair from 00-99 but no repeats (00 01, 00 02, 00 03, ...
-=======
+
  *main - print a num pair from 00-99 but no repeats (00 01, 00 02, 00 03,...)
->>>>>>> 8c711c279187b096659f8248216f3b28e121499b
+
  *Return: Always 0 (Success)
+
  */
 
+
+
 int main(void)
+
 {
+
 	int tens;
+
 	int ones;
+
 	int t;
+
 	int o;
 
+
+
 	for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
+
 	{
+
 		for (ones = '0'; ones <= '9'; ones++)
+
 		{
+
 		 	for (t = tens; t <= '9'; t++) /*print second of pair*/
+
 			{
+
 				for (o = ones + 1; o <= '9'; o++)
+
 				{
+
 					putchar(tens);
+
 					putchar(ones);
+
 					putchar(' ');
+
 					putchar(t);
+
 					putchar(o);
 
-					if (!((tens == '9' && ones == '8') &&
-					      (t == '9' && o == '9')))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				o = '0';
-			}
-		}
-	}
-	putchar('\n');
-<<<<<<< HEAD
-=======
 
->>>>>>> 8c711c279187b096659f8248216f3b28e121499b
+
+					if (!((tens == '9' && ones == '8') &&
+
+					      (t == '9' && o == '9')))
+
+					{
+
+						putchar(',');
+
+						putchar(' ');
+
+					}
+
+				}
+
+				o = '0';
+
+			}
+
+		}
+
+	}
+
+	putchar('\n');
+
+
+
 	return (0);
+
 }

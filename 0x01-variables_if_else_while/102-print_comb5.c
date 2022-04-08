0b81b8db2e62bc1b095b92fb746fe675fb9c8eee
vantwill <101398976+vantwill@users.noreply.github.com>
2022-04-08 13:28:53 -0700

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,11 +11,11 @@ int main(void)
 	int ten;
 	int hundred;
 
-	for (hundred = '0'; hundred <= '9'; hundred++) /*hundreds place*/
+	for (hundred = '0'; hundred <= '9'; hundred++)        /*hundreds place*/
 	{
-		for (ten = (hundred + 1); ten <= '9'; ten++) /*tens=100s+1*/
+		for (ten = (hundred + 1); ten <= '9'; ten++)     /*tens=100s+1*/
 		{
-			for (one = (ten + 1); one <= '9'; one++) /*ones*/
+			for (one = (ten + 1); one <= '9'; one++)    /*ones*/
 			{
 				putchar(hundred);
 				putchar(ten);

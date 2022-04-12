fe7cc15256179f2f6a1d66c96a2499d3d29e87c2
vantwill <101398976+vantwill@users.noreply.github.com>
2022-04-12 06:51:45 -0700

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,49 +1,51 @@
 #include "main.h"
 
 /**
- * print_times_table - print multiplication table up to n
- * @n: integer argument
+ * _printstr - prints a given string
+ * @c: a null temrinated character array
+ *
+ * Return: 0 if successful
  */
+int _printstr(char c[])
+{
+	int i = 0;
 
+	while (c[i] != '\0')
+		_putchar(c[i++]);
+
+	return (0);
+}
+
+/**
+ * print_times_table - prints out an arbitrary multiplication table between
+ * 1 and 15
+ *
+ * @n: integer that defines dimnsions for the table
+ *
+ * Return void
+ */
 void print_times_table(int n)
 {
-	int row;
-	int column;
-	int product;
-	if (n >= 0 && n <= 15)
+	char buffer[5] = ",    ";
+	int i, j, num;
+
+	if (n < 0 || n > 15)
+		return;
+	for (i = 0; i <= n; i++)
 	{
-		for (row = 0; row <= n; row++)
+		_putchar('0');
+		for (j = 1; j <= n; j++)
 		{
-			for (column = 0; column <= n; column++)
-			{
-				product = (row * column);
-				if (column == 0)
-					_putchar('0' + product);
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-					if (product <= 9)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar('0' + product);
-					}
-					else if (product > 9 && product < 100)
-					{
-						_putchar(' ');
-						_putchar('0' + (product / 10));
-						_putchar('0' + (product % 10));
-					}
-					else if (product >= 100)
-					{
-						_putchar('0' + (product / 100));
-						_putchar('0' + ((product / 10) % 10));
-						_putchar('0' + (product % 10));
-					}
-				}
-			}
-			_putchar('\n');
+			num = i * j;
+			buffer[2] = num / 100 + 48;
+			buffer[3] = num / 10 % 10 + 48;
+			buffer[4] = num % 10 + 48;
+			if (buffer[2] == '0')
+				buffer[2] = ' ';
+			if (buffer[3] == '0' && buffer[2] == ' ')
+				buffer[3] = ' ';
+			_printstr(buffer);
 		}
+		_putchar('\n');
 	}
 }

2ab48f52b89db18d07c2bbf7a92f3510f79f6cb8
vantwill <101398976+vantwill@users.noreply.github.com>
2022-04-13 00:12:04 -0700

    main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - main 
+ * main - main
  * Return: 0
  */
 
@@ -8,9 +8,9 @@ int main(void)
 {
 int i;
 i = 0;
-  
+
 positive_or_negative(i);
-  
+
 return (0);
-  
+
 }

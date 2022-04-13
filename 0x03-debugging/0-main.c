efa5324774535d8fcfd3e22c7bc2b581599681b9
vantwill <vtwill41@gmail.com>
2022-04-12 23:59:39 -0700

    Main c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+
+/**
+
+ * main - main entry
+
+ * Return: 0
+
+ */
+
+
+
+int main(void)
+
+{
+
+int i;
+
+i = 0;
+
+
+
+positive_or_negative(i);
+
+
+
+return (0);
+
+
+
+}

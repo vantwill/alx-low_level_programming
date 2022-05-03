a962789981f07ff77ce16a4acc09937c249a3a07
vantwill <101398976+vantwill@users.noreply.github.com>
2022-05-03 01:35:29 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,3 +1,13 @@
+#include "main.h"
+/**
+ *create_array - array for prints a string
+ *@size: number elements array
+ *@c: char
+ *Return: pointer
+ */
+
+char *create_array(unsigned int size, char c)
+{
 char *buffer;
 unsigned int position;
 

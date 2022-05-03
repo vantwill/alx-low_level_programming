f555738b43803d96ab74cb7ea8ea3df822c53805
vantwill <101398976+vantwill@users.noreply.github.com>
2022-05-03 01:31:47 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,58 +1,29 @@
 char *buffer;
-
 unsigned int position;
 
-
-
 if (size == 0)
-
 {
-
 return (NULL);
-
 }
-
-
 
 /*Define values with malloc*/
-
 buffer = (char *) malloc(size * sizeof(c));
 
-
-
 if (buffer == 0)
-
 {
-
 return (NULL);
-
 }
-
-
 
 else
-
 {
-
 position = 0;
-
 while (position < size) /*While for array*/
-
 {
-
 *(buffer + position) = c;
-
 position++;
-
 }
-
-
 
 return (buffer);
-
 }
 
-
-
 }
-

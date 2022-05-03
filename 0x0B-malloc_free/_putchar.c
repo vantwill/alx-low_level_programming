15e1381cbf272df6e8e17433c23dd808a128484a
vantwill <101398976+vantwill@users.noreply.github.com>
2022-05-03 01:30:56 -0700

    _putchar.c

--- 0x0B-malloc_free/_putchar.c
+++ 0x0B-malloc_free/_putchar.c
@@ -1,26 +1,13 @@
 #include <unistd.h>
 
-
-
 /**
-
- * _putchar - writes the character c to stdout
-
- * @c: The character to print
-
+ * _putchar - van twillwrites the character c to stdout 
+ * @c: The character to print fill
  *
-
- * Return: On success 1.
-
+ * Return: On success 1.great
  * On error, -1 is returned, and errno is set appropriately.
-
  */
-
 int _putchar(char c)
-
 {
-
 return (write(1, &c, 1));
-
 }
-

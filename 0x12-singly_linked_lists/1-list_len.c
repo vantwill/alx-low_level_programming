ba6364c28383ac144e77a8ca5e69880d52d7ca64
vantwill <101398976+vantwill@users.noreply.github.com>
2022-05-19 02:37:11 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,28 +1,20 @@
 #include "lists.h"
 
 /**
-
  * list_len - returns then number of elements in a list.
-
  * @h: singly linked list.
-
  * Return: number of elements in the list.
-
  */
 
-
-
 size_t list_len(const list_t *h)
-
 {
+    size_t nelem;
 
-    size_t van;
-
-    van = 0;
-    while (h != NULL
-   {
-       h = h->next;
-       tews++;
-   }
-   return (van);
+    nelem = 0;
+    while (h != NULL)
+    {
+        h = h->next;
+        nelem++;
+    }
+    return (nelem);
 }

ce0dd6e8323abed6496b6c41423757e44ce09309
vantwill <101398976+vantwill@users.noreply.github.com>
2022-05-19 04:24:38 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	size_t nodess = 0;
 
 	while (h)
 	{
@@ -20,9 +20,9 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
-		nodes++;
+		nodess++;
 		h = h->next;
 	}
 
-	return (nodes);
+	return (nodess);
 }

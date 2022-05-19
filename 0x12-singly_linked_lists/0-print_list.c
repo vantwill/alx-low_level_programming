ae647c529e63da7ad87ec539d407e17fdef5a210
vantwill <101398976+vantwill@users.noreply.github.com>
2022-05-19 02:10:07 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,6 +4,7 @@
  *print_list - print all element
  *Return number of element
  */
+
 size_t print_list(const list_t *h)
 {
 size_t tews;
@@ -11,14 +12,10 @@ tews = 0;
 while (h != NULL)
 {
 if (h->str == NULL)
-
 printf("[%d] %s\n", 0, "(nil)");
-
 else
 printf("[%d] %s\n", h->len, h->str);
-
 h = h->next;
-
 tews++;
 }
 return (tews);

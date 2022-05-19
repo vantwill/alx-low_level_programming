09f3eabb0cbc76f32ae8e35656f54eed11a429b8
vantwill <101398976+vantwill@users.noreply.github.com>
2022-05-19 02:13:04 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,8 +7,8 @@
 
 size_t print_list(const list_t *h)
 {
-size_t tews;
-tews = 0;
+size_t nelem;
+nelem = 0;
 while (h != NULL)
 {
 if (h->str == NULL)
@@ -16,7 +16,7 @@ printf("[%d] %s\n", 0, "(nil)");
 else
 printf("[%d] %s\n", h->len, h->str);
 h = h->next;
-tews++;
+nelem++;
 }
 return (tews);
 }

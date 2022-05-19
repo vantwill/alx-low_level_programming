67eca4310c18505e6df3456347c9d55b5bb7f334
vantwill <vtwill41@gmail.com>
2022-05-19 11:27:57 -0700

    commit

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+#include <stdlib.h>
+
+
+
+/**
+
+ * free_list - Frees a list_t list.
+
+ * @head: A pointer to the list_t list.
+
+ */
+
+void free_list(list_t *head)
+
+{
+
+	list_t *curt;
+
+
+
+	while (head)
+
+	{
+
+		curt = head->next;
+
+		free(head->str);
+
+		free(head);
+
+		head = curt;
+
+	}
+
+}
+

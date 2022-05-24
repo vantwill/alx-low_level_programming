2446a1b314845d6317a0df8c007f9a4bc76b27ee
vantwill <101398976+vantwill@users.noreply.github.com>
2022-05-24 00:21:26 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,17 +1,17 @@
 #include "lists.h"
 
 /**
- *
+ * free_listint - Frees a listint_t list.
+ * @head: A pointer to the head of the listint_t list to be freed.
  */
-
 void free_listint(listint_t *head)
 {
-  listint_t *crt;
-  while (head)
-  {
-   crt = head->next;
-   free(head);
-   head = tmp;
+	listint_t *crt;
 
-}
+	while (head)
+	{
+		crt = head->next;
+		free(head);
+		head = crt;
+	}
 }

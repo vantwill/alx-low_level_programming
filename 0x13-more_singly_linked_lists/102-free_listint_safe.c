72d3102b459eeed04d5f1e30cb77a7c3b2b8b95e
vantwill <vtwill41@gmail.com>
2022-05-24 01:30:36 -0700

    commit

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,193 @@
+#include "lists.h"
+
+
+
+size_t looped_listint_count(listint_t *head);
+
+size_t free_listint_safe(listint_t **h);
+
+
+
+/**
+
+ * looped_listint_count - Counts the number of unique nodes
+
+ *                      in a looped listint_t linked list.
+
+ * @head: A pointer to the head of the listint_t to check.
+
+ *
+
+ * Return: If the list is not looped - 0.
+
+ *         Otherwise - the number of unique nodes in the list.
+
+ */
+
+size_t looped_listint_count(listint_t *head)
+
+{
+
+	listint_t *trtoise, *hare;
+
+	size_t nod = 1;
+
+
+
+	if (head == NULL || head->next == NULL)
+
+		return (0);
+
+
+
+	trtoise = head->next;
+
+	hare = (head->next)->next;
+
+
+
+	while (hare)
+
+	{
+
+		if (trtoise == hare)
+
+		{
+
+			trtoise = head;
+
+			while (trtoise != hare)
+
+			{
+
+				nod++;
+
+				trtoise = trtoise->next;
+
+				hare = hare->next;
+
+			}
+
+
+
+			trtoise = trtoise->next;
+
+			while (tortoise != hare)
+
+			{
+
+				nod++;
+
+				trtoise = trtoise->next;
+
+			}
+
+
+
+			return (nod);
+
+		}
+
+
+
+		trtoise = trtoise->next;
+
+		hare = (hare->next)->next;
+
+	}
+
+
+
+	return (0);
+
+}
+
+
+
+/**
+
+ * free_listint_safe - Frees a listint_t list safely (ie.
+
+ *                     can free lists containing loops)
+
+ * @h: A pointer to the address of
+
+ *     the head of the listint_t list.
+
+ *
+
+ * Return: The size of the list that was freed.
+
+ *
+
+ * Description: The function sets the head to NULL.
+
+ */
+
+size_t free_listint_safe(listint_t **h)
+
+{
+
+	listint_t *tmp;
+
+	size_t nod, index;
+
+
+
+	nod = looped_listint_count(*h);
+
+
+
+	if (nod == 0)
+
+	{
+
+		for (; h != NULL && *h != NULL; nod++)
+
+		{
+
+			tmp = (*h)->next;
+
+			free(*h);
+
+			*h = tmp;
+
+		}
+
+	}
+
+
+
+	else
+
+	{
+
+		for (index = 0; index < nod; index++)
+
+		{
+
+			tmp = (*h)->next;
+
+			free(*h);
+
+			*h = tmp;
+
+		}
+
+
+
+		*h = NULL;
+
+	}
+
+
+
+	h = NULL;
+
+
+
+	return (nod);
+
+}
+
+

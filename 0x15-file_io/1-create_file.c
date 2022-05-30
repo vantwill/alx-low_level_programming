09f3e445479f467343b9345595fc904597f8eda2
vantwill <vtwill41@gmail.com>
2022-05-30 06:06:40 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+/*
+ * File: 1-create_file.c
+ * Auth: Tewolde
+ */
+
+#include "main.h"
+
+/**
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file, fwrite, i = 0;
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i];)
+			i++;
+	}
+
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fwrite = write(file, text_content, i);
+
+	if (file == -1 || fwrite == -1)
+		return (-1);
+
+	close(file);
+
+	return (1);
+}
+
+

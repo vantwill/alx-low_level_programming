7545920a14e455eb3807bda50d21f8aac18c01db
vantwill <vtwill41@gmail.com>
2022-11-02 06:12:25 -0700

    1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,64 +1,37 @@
 #include "search_algos.h"
-/**
- * print_array - funti
- * @array: array to print
- * @limt_low: point to start print
- * @limt_high: point to stop
- */
-void print_array(int *array, int limt_low, int limt_high)
-{
-	char *a = ", ";
-	int t = 0;
-
-	if (limt_lo < limt_high)
-	{
-		printf("Searching in array: ");
-		for (t = limt_lo; t <= limt_high; t++)
-		{
-			printf("%d", array[t]);
-			if (t < limt_high)
-				printf("%s", a);
-		}
-		printf("\n");
-	}
-}
 
 /**
- * binary_search - funtion for binary search
- * @array: array to make search inside
- * @size: length if the array
- * @value: value to search in the array for binary search
- * Return: Always a int number
+ * binary_search - searches 
+ * the Binary search algorithm
+ * @array: pointer to 
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: the first index where value is located or -1
  */
-
 int binary_search(int *array, size_t size, int value)
 {
-	int lo = 0;
-	int high = size - 1;
-	int midle = high / 2;
-
-	if (array == NULL)
+	size_t j, k, m, r;
+	k = 0;
+	r = size - 1;
+	if (!array)
 		return (-1);
-
-	while (lo <= high)
+	while (k <= r)
 	{
-		print_array(array, lo, high);
-		
-		if (array[midle] == value)
+		m = (k + r) / 2;
+		printf("Searching in array:");
+		for (j = k; j <= r; j++)
 		{
-			if (midle != lo)
-				print_array(array, midle, high);
-			return (midle);
+			if (j != r)
+				printf(" %d,", array[j]);
+			else
+				printf(" %d\n", array[j]);
 		}
-
-		else if (array[midle] < value)
-			lo = midle + 1;
+		if (array[m] < value)
+			k = m + 1;
+		else if (array[m] > value)
+			r = m - 1;
 		else
-			high = midle - 1;
-
-		midle = lo + (high - lo) / 2;
+			return (m);
 	}
-	printf("Searching in array: %d\n", array[high]);
-
 	return (-1);
 }

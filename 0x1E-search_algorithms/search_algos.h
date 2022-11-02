9bd624aa2dda46a08d3b9963b63cc589f8237b31
vantwill <vtwill41@gmail.com>
2022-11-02 05:03:21 -0700

    search_algos.h

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,16 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/* Estand librery includes */
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* Prototype of funtions */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+
+#endif /* _SERACH_ALGOS_H_ */
